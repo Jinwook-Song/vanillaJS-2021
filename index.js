bd92f82ef4082e92a17fe90b3e99f6264598925a
Jinwook-Song <wlsdnr129@naver.com>
2021-03-05 15:18:43 +0900

    lecture 2.5~2.6

---
+++ index.js
@@ -0,0 +1,31 @@
+const title = document.querySelector("#title")
+
+const BASE_COLOR = "azure"
+const OTHER_COLOR = "#7f8c8d"
+
+
+function handleClick() {
+    const currentColor = title.style.color;
+    if (currentColor === BASE_COLOR){
+        title.style.color = OTHER_COLOR;
+    } else {
+        title.style.color = BASE_COLOR;
+    }
+
+}
+
+function init(){
+    title.style.color = BASE_COLOR;
+    title.addEventListener("mouseenter", handleClick);
+}
+
+init();
+
+function handleOnline() {
+    console.log("welcom");
+}
+
+window.addEventListener("online", handleOnline);
+
+
+
